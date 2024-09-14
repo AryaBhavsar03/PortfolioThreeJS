aab7f49097ad29dd84909a70fc6b65a1f16dfd62
Arya <Aryabhavsar2003@gmail.com>
2024-09-14 18:17:06 -0400

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio-three-js/",
+  base: '/portfolio-three-js/',
 })
